bb1f107bdfa6d8a0a1599de149b27c1d02ab8224
STUNC <tunc0687@hotmail.com>
2020-06-23 23:08:18 +0300

    api movies işlemleri yapıldı.

---
+++ helper/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    mongoose.connect("mongodb://stunc:sd1234@ds149218.mlab.com:49218/heroku_467c8966", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const db = mongoose.connection;
+    db.on('error', () => {
+        console.log("MongoDB bağlantı hatası.");
+    });
+    db.once('open', () => {
+        console.log("MongoDB bağlantı sağlandı.");
+    });
+
+    mongoose.Promise = global.Promise;
+};
+
---
+++ routes/movies.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+
+const Movie = require("../models/Movie");
+
+// Yeni movie ekle
+router.post('/', (req, res, next) => {
+    const movie = new Movie(req.body);
+    const promise = movie.save();
+    promise.then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Tüm movie leri getir
+router.get('/', (req, res, next) => {
+    const promise = Movie.find({});
+    promise.then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// imdb score a göre top10 movie yi getirir
+router.get('/top10', (req, res, next) => {
+    const promise = Movie.find({})
+        .sort({imdb_score: -1})
+        .limit(10);
+    promise.then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Between year
+router.get('/between/:start_year/:end_year', (req, res, next) => {
+    const {start_year, end_year} = req.params;
+    const promise = Movie.find(
+        {
+            year: {"$gte" : start_year , "$lte" : end_year }
+        }
+    );
+    promise.then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Id ye göre istenen movie yi getir
+router.get('/:movie_id', (req, res, next) => {
+    const mongodbregex = /^[0-9a-fA-F]{24}$/;
+
+    if (req.params.movie_id.match(mongodbregex)) {
+        const promise = Movie.findById(req.params.movie_id);
+        promise.then(movie => {
+            if (!movie) {
+                next({ message: "The movie is not found!" });
+            } else {
+                res.json(movie); 
+            }
+            
+        }).catch(err => res.json(err));
+    } else {
+        next({ message: "Geçerli bir id diriniz." });
+    }
+});
+
+// Id ye göre movie yi bulup güncellemek için kullanılır
+router.put('/:movie_id', (req, res, next) => {
+    const mongodbregex = /^[0-9a-fA-F]{24}$/;
+
+    if (req.params.movie_id.match(mongodbregex)) {
+        const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new: true});
+        promise.then(movie => {
+            if (!movie) {
+                next({ message: "The movie is not found!" });
+            } else {
+                res.json(movie); 
+            }
+            
+        }).catch(err => res.json(err));
+    } else {
+        next({ message: "Geçerli bir id diriniz." });
+    }
+});
+
+// Id ye göre movie yi silmek için kullanılır
+router.delete('/:movie_id', (req, res, next) => {
+    const mongodbregex = /^[0-9a-fA-F]{24}$/;
+
+    if (req.params.movie_id.match(mongodbregex)) {
+        const promise = Movie.findByIdAndRemove(req.params.movie_id);
+        promise.then(movie => {
+            if (!movie) {
+                next({ message: "The movie is not found!" });
+            } else {
+                res.json({status : 1}); 
+            }
+            
+        }).catch(err => res.json(err));
+    } else {
+        next({ message: "Geçerli bir id diriniz." });
+    }
+});
+
+
+
+module.exports = router;
